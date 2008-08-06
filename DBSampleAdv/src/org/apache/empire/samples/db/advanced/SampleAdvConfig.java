1890af2dbcb844b014919dafc9a54328d7090b74
Rainer Doebele <doebele@apache.org>
2008-08-06 09:07:15 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/empire-db/trunk/core@683186 13f79535-47bb-0310-9956-ffa450edef68

---
+++ DBSampleAdv/src/org/apache/empire/samples/db/advanced/SampleAdvConfig.java
@@ -0,0 +1,104 @@
+package org.apache.empire.samples.db.advanced;
+
+import org.apache.empire.xml.XMLConfiguration;
+
+/**
+ * <PRE>
+ * The SampleConfig class provides access to configuration settings.
+ * The configruation will be read from a xml configuration file (usually config.xml) 
+ * Thus the default values here will be overridden. 
+ * </PRE>
+ */
+public class SampleAdvConfig extends XMLConfiguration
+{
+
+    private String databaseProvider = "hsqldb";
+
+    private String jdbcClass = "org.hsqldb.jdbcDriver";
+
+    private String jdbcURL = "jdbc:hsqldb:file:hsqldb/sample;shutdown=true";
+
+    private String jdbcUser = "jdbc:hsqldb:file:hsqldb/sample;shutdown=true";
+
+    private String jdbcPwd = "";
+
+    private String schemaName = "DBSAMPLE";
+
+    /**
+     * Initialize the configuration.
+     */
+    public boolean init(String filename)
+    {
+        // Read the properties file
+        if (super.init(filename, false, true) == false)
+            return false;
+        // Done
+        if (readProperties(this, "properties")==false)
+            return false;
+        // Reader Provider Properties
+        return readProperties(this, "properties-" + databaseProvider);
+    }
+
+    public String getDatabaseProvider()
+    {
+        return databaseProvider;
+    }
+
+    public String getJdbcClass()
+    {
+        return jdbcClass;
+    }
+
+    public void setJdbcClass(String jdbcClass)
+    {
+        this.jdbcClass = jdbcClass;
+    }
+
+    public String getJdbcPwd()
+    {
+        return jdbcPwd;
+    }
+
+    public void setJdbcPwd(String jdbcPwd)
+    {
+        this.jdbcPwd = jdbcPwd;
+    }
+
+    public String getJdbcURL()
+    {
+        return jdbcURL;
+    }
+
+    public String getSchemaName()
+    {
+        return schemaName;
+    }
+
+    // ------- Setters -------
+
+    public void setDatabaseProvider(String databaseProvider)
+    {
+        this.databaseProvider = databaseProvider;
+    }
+
+    public void setJdbcURL(String jdbcURL)
+    {
+        this.jdbcURL = jdbcURL;
+    }
+
+    public String getJdbcUser()
+    {
+        return jdbcUser;
+    }
+
+    public void setJdbcUser(String jdbcUser)
+    {
+        this.jdbcUser = jdbcUser;
+    }
+
+    public void setSchemaName(String schemaName)
+    {
+        this.schemaName = schemaName;
+    }
+
+}
