9031f88d25b4c9caae05f5d03db130ac1300b52f
Joerg Reiher <reiher@apache.org>
2008-09-11 11:22:39 +0000

    changed names, added Apache header
    git-svn-id: https://svn.apache.org/repos/asf/incubator/empire-db/trunk@694217 13f79535-47bb-0310-9956-ffa450edef68

--- struts2-ext/Empire-struts2-ext/dist/copy_lib.bat
+++
@@ -1,6 +0,0 @@
-REM Apache Empire-struts2-ext
-REM Copyright 2008 The Apache Software Foundation
-REM *********************************************
-ECHO Copy jar files form lib direcotry to this directory
-copy ..\..\..\..\..\lib\*.jar .
-copy ..\..\..\..\..\lib\libinfo.txt .
