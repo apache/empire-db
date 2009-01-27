450ffbb95247eb9388d8503c19729d1a73034748
Francis De Brabandere <francisdb@apache.org>
2009-01-27 22:30:26 +0000

    removed unused import
    git-svn-id: https://svn.apache.org/repos/asf/incubator/empire-db/branches/maven@738283 13f79535-47bb-0310-9956-ffa450edef68

--- empire-db/src/test/java/org/apache/empire/commons/ObjectUtilsTest.java
+++ empire-db/src/test/java/org/apache/empire/commons/ObjectUtilsTest.java
@@ -25,10 +25,8 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 
 import org.junit.Ignore;
