41607b469c09c65994b015de7f3b02daa20fe3f6
Francis De Brabandere <francisdb@apache.org>
2009-09-05 14:04:45 +0000

    generate the rat report after doing mvn clean to make sure the target paths are not scanned
    git-svn-id: https://svn.apache.org/repos/asf/incubator/empire-db/trunk@811650 13f79535-47bb-0310-9956-ffa450edef68

--- release.sh
+++ release.sh
@@ -60,13 +60,13 @@ done
 echo "Clean all projects"
 mvn5 clean -Pall
 
+echo "Generating rat report"
+mvn5 rat:check -Drat.excludeSubprojects=false
+
 # package and assemble the release
 echo "Package and assemble the release"
 # mvn5 -ff -Dgpg.passphrase="$passphrase" -Prelease deploy javadoc:aggregate assembly:attached $1
 mvn5 -ff -Dgpg.passphrase="$passphrase" clean install javadoc:aggregate assembly:attached $1
-
-echo "Generating rat report"
-mvn5 rat:check -Drat.excludeSubprojects=false
 
 filename=`ls target/dist/apache-empire*gz`
 md5sum $filename > $filename.md5
