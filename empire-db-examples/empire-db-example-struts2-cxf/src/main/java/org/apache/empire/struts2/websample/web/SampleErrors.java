8c9a341517765b865486f276b0e65e5aa33b68be
Francis De Brabandere <francisdb@apache.org>
2011-08-16 13:16:58 +0000

    EMPIREDB-99 Refactor struts2 cxf example for new error handling
    git-svn-id: https://svn.apache.org/repos/asf/incubator/empire-db/branches/EMPIREDB-99@1158260 13f79535-47bb-0310-9956-ffa450edef68

---
+++ empire-db-examples/empire-db-example-struts2-cxf/src/main/java/org/apache/empire/struts2/websample/web/SampleErrors.java
@@ -0,0 +1,11 @@
+/*
+ * ESTEAM Software GmbH, 16.08.2011
+ */
+package org.apache.empire.struts2.websample.web;
+
+import org.apache.empire.commons.ErrorType;
+
+public class SampleErrors
+{
+    public static final ErrorType InvalidPassword = new ErrorType("error.invalidpassword", "Invalid Password.");
+}
