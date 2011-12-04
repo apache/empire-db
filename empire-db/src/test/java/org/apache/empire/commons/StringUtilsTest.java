ed4b55f7d2c8e4656299d204863e9c54337618e4
Rainer Doebele <doebele@apache.org>
2011-12-04 18:34:16 +0000

    EMPIREDB-126 bugfix in test
    git-svn-id: https://svn.apache.org/repos/asf/incubator/empire-db/trunk@1210172 13f79535-47bb-0310-9956-ffa450edef68

--- empire-db/src/test/java/org/apache/empire/commons/StringUtilsTest.java
+++ empire-db/src/test/java/org/apache/empire/commons/StringUtilsTest.java
@@ -103,7 +103,7 @@ public class StringUtilsTest
 		assertEquals(null, StringUtils.arrayToString(new String[]{} , ""));
 		assertEquals("test", StringUtils.arrayToString(new String[]{"test"} , "|"));
 		assertEquals("12312.3", StringUtils.arrayToString(new Number[]{Integer.valueOf("123"), Double.valueOf("12.3")} , ""));
-		assertEquals("firstnullsecondnullthird", StringUtils.arrayToString(new String[]{"first", "second", "third"} , null));
+		assertEquals("firstsecondthird", StringUtils.arrayToString(new String[]{"first", "second", "third"} , null));
 		assertEquals(" first \t second \t third ", StringUtils.arrayToString(new String[]{" first ", " second ", " third "} , "\t"));
 		assertEquals("null/null", StringUtils.arrayToString(new String[]{null, null} , "/"));
 		assertEquals("null", StringUtils.arrayToString(new String[]{null} , "/"));
