fc7b248808e06b38105c55c63607c33b14b54a22
Rainer Döbele <doebele@apache.org>
2022-02-24 16:32:00 +0100

    EMPIREDB-362 GenerateExample updated

---
+++ empire-db-examples/empire-db-example-codegen/src/main/java/GenerateExample.java
@@ -0,0 +1,28 @@
+import org.apache.empire.db.codegen.CodeGenConfig;
+import org.apache.empire.db.codegen.CodeGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * GenerateExample
+ * Simple wrapper to call the CodeGenerator
+ * see generate-config.xml for details
+ * @author doebele
+ */
+public final class GenerateExample
+{
+    private static final Logger log = LoggerFactory.getLogger(GenerateExample.class);
+
+    public static void main(String[] args)
+    {
+        CodeGenConfig config = new CodeGenConfig();
+        config.init("generate-config.xml");
+        
+        log.info("Creating code for {}", config.getJdbcURL());
+        
+        CodeGenerator app = new CodeGenerator();
+        app.generate(config);
+        
+        log.info("Code generation complete. File are located in {}", config.getTargetFolder());
+    }
+}
