7c8180a1edbffc158e3538b1e87c39986ee79d48
Rainer Döbele <rdoebele@users.noreply.github.com>
2024-03-24 12:19:28 +0100

    EMPIREDB-419 XMLConfiguration: Support Variables and Map properties

--- empire-db/src/main/java/org/apache/empire/xml/XMLConfiguration.java
+++ empire-db/src/main/java/org/apache/empire/xml/XMLConfiguration.java
@@ -23,15 +23,21 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.empire.commons.ObjectUtils;
 import org.apache.empire.commons.StringUtils;
+import org.apache.empire.exceptions.EmpireException;
 import org.apache.empire.exceptions.FileParseException;
 import org.apache.empire.exceptions.FileReadException;
 import org.apache.empire.exceptions.InternalException;
@@ -61,6 +67,32 @@ public class XMLConfiguration
     private static final Logger log = LoggerFactory.getLogger(XMLConfiguration.class);
 
     private Element configRootNode = null;
+
+    /*
+     * Variable prefix and suffix 
+     */
+    protected final String VAR_BEGIN;
+    protected final char   VAR_END;
+    protected final char   VAR_DEFAULT;
+
+    /**
+     * Standard Constructor without variable support
+     */
+    public XMLConfiguration()
+    {
+        this(null); // No variable support for legacy purposes
+    }
+    
+    /**
+     * Constructor allowing to specify a variable prefix (e.g. $ or #)
+     * @param varIndicator the variable prefix
+     */
+    public XMLConfiguration(String varIndicator)
+    {
+        this.VAR_BEGIN = (varIndicator!=null ? (varIndicator.endsWith("{") ? varIndicator : varIndicator+"{") : null); 
+        this.VAR_END   = '}';
+        this.VAR_DEFAULT = '|';
+    }
 
     /**
      * Initialize the configuration.
@@ -185,11 +217,12 @@ public class XMLConfiguration
             if (item.getNodeType() != Node.ELEMENT_NODE)
                 continue;
             // Get the Text and set the Property
-            if (isProperty(item))
-                setPropertyValue(bean, item);
+            if (item instanceof Element)
+                setPropertyValue(bean, (Element)item);
         }
     }
     
+    /*
     protected boolean isProperty(Node item)
     {
         NamedNodeMap map = item.getAttributes();
@@ -203,36 +236,228 @@ public class XMLConfiguration
             return true; // assume yes
         return ObjectUtils.getBoolean(value);
     }
+    */
     
-    protected void setPropertyValue(Object bean, Node item)
+    /**
+     * Sets the property value of an XML Element 
+     * @param bean the java bean containing the property 
+     * @param item the configuration node
+     */
+    @SuppressWarnings("unchecked")
+    protected void setPropertyValue(Object bean, Element item)
     {
-        // Get the Text and set the Property
         String name = item.getNodeName();
+        String prop = name;
         try
         {
-            String newValue = XMLUtil.getElementText(item);
-            BeanUtils.setProperty(bean, name, newValue);
-
-            Object value = BeanUtils.getProperty(bean, name);
-            if (ObjectUtils.compareEqual(newValue, value))
-            {
-            	log.info("Configuration property '{}' set to \"{}\"", name, newValue);
+            Class<?> valueType = PropertyUtils.getPropertyType(bean, name);
+            // Has Attributes?
+            if (item.hasAttributes() && Map.class.isAssignableFrom(valueType))
+            {   // It's a map type
+                Object value = PropertyUtils.getNestedProperty(bean, name);
+                Map<String,Object> map;
+                if (value instanceof Map)
+                {   // Use existing
+                    map = (Map<String,Object>)value;
+                }
+                else
+                {   // Create and Set
+                    map = new HashMap<String, Object>();
+                    PropertyUtils.setProperty(bean, name, map);
+                }
+                // copy to map
+                NamedNodeMap nm = item.getAttributes();
+                for (int i = 0; i<nm.getLength(); i++)
+                {
+                    Node attrib = nm.item(i);
+                    prop = attrib.getNodeName();
+                    String propVal = attrib.getNodeValue();
+                    map.put(prop, resolveValue(valueType, propVal));
+                    log.info("Configuration Map property '{}' has been set to \"{}\"", prop, map.get(prop));
+                }
+                return; 
             }
-            else
-            {
-            	log.error("Failed to set property '{}'. Value is \"{}\"", name, value);
-            }
-
-        } catch (IllegalAccessException e)
-        {
-            log.error("Access to Property {} denied.", name);
-        } catch (InvocationTargetException e)
-        {
-            log.error("Unable to set Property {}", name);
-        } catch (NoSuchMethodException e)
-        {
-            log.warn("Property '{}' not found in {}. Will be ingored.", name, bean.getClass().getName());
+            // default
+            Object newValue = resolveValue(valueType, XMLUtil.getElementText(item));
+            setPropertyValue(bean, name, newValue);
+        } catch (IllegalAccessException e) {
+            log.error("Config error: Access to '{}' in {} denied.", prop, bean.getClass().getName(), e);
+        } catch (InvocationTargetException e) {
+            log.error("Config error: Unable to set value for '{}' in {}", prop, bean.getClass().getName(), e);
+        } catch (NoSuchMethodException e) {
+            log.error("Config error: Property '{}' in {} not found. Property is ignored.", prop, bean.getClass().getName(), e);
+        } catch (EmpireException e) {
+            log.error("Config error: Invalid Value for '{}' in {}", prop, bean.getClass().getName(), e);
         }
+    }
+    
+    /**
+     * Sets a property value
+     * @param bean the name of the object
+     * @param name the name of the property
+     * @param value the value
+     */
+    protected void setPropertyValue(Object bean, String name, Object value)
+        throws IllegalAccessException, InvocationTargetException, NoSuchMethodException
+    {
+        // This method provides type conversion
+        BeanUtils.setProperty(bean, name, value);
+
+        // Check
+        // Object newValue = BeanUtils.getProperty(bean, name);
+        Object newValue = PropertyUtils.getProperty(bean, name);
+        if (ObjectUtils.compareEqual(newValue, value))
+        {   // Success
+            log.info("Configuration property '{}' has been set to \"{}\"", name, value);
+        }
+        else
+        {   // Property value does not match
+            log.error("Config error: Failed to set '{}' to \"{}\" in {}. Value is \"{}\" instead.", name, value, bean.getClass().getName(), value);
+        }
+    }
+    
+    /**
+     * Resolves a property value by replacing variables and type conversion
+     * @param valueType the destination type
+     * @param value the config value
+     * @return the property value
+     */
+    protected Object resolveValue(Class<?> valueType, String value)
+    {
+        if (value==null)
+            return null;
+        int beg = (StringUtils.isNotEmpty(VAR_BEGIN) ? value.indexOf(VAR_BEGIN) : -1);
+        if (beg<0)
+            return value;
+        // resolve variable
+        int end = findVarEnd(value, beg+2);
+        if (end<0)
+            throw new InvalidArgumentException("value", value);
+        // resolve
+        String defaultValue = null; 
+        String var = value.substring(beg+2, end);
+        int def = var.indexOf(VAR_DEFAULT);
+        if (def>0)
+        {   // Default value provided
+            defaultValue = (String)resolveValue(String.class, var.substring(def+1));
+            var = var.substring(0, def);
+        }
+        String varVal = resolveVariable(var, defaultValue);
+        if (varVal==null)
+            throw new ItemNotFoundException(var);
+        StringBuilder b = new StringBuilder(value.length()+varVal.length()-(end-beg)-1);
+        b.append(value.substring(0, beg));
+        b.append(varVal);
+        b.append(value.substring(end+1));
+        return resolveValue(valueType, b.toString());
+    }
+
+    /**
+     * Returns the value for a given variable
+     * If a variable value cannot be resolved the function should return the default value
+     * @param var the variable name
+     * @param defaultValue the default value or null
+     * @return the variable value
+     */
+    protected String resolveVariable(String var, String defaultValue)
+    {
+        log.debug("Config: Resolving variable {}", var);
+        String value = System.getProperty(var);
+        return (value!=null ? value : defaultValue);
+    }
+    
+    /* Implementation of toString */
+    
+    private static String EOL = "\r\n";
+    private static String EOC = "----------------------------------------\r\n";  // end of class
+    
+    @Override
+    public String toString()
+    {
+        StringBuilder b = new StringBuilder(EOC);
+        Class<?> clazz = getClass();
+        while (clazz!=null && clazz!=XMLConfiguration.class)
+        {
+            appendConfigProperties(clazz, this, b, true);
+            clazz = clazz.getSuperclass();
+        }
+        return b.toString();
+    }
+    
+    protected void appendConfigProperties(Class<?> clazz, Object object, StringBuilder b, boolean appendClassInfo)
+    {
+        Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            // ignore static fields
+            int modifiers = field.getModifiers();
+            if (Modifier.isStatic(modifiers) || Modifier.isFinal(modifiers))
+                continue;
+            // make accessible
+            try
+            {
+                Object value = PropertyUtils.getProperty(object, field.getName());
+                if (value!=null && !(value instanceof Map<?,?>))
+                {
+                    Class<?> vc = value.getClass();
+                    boolean simple = (vc.isPrimitive() || vc.isEnum() 
+                         || vc == String.class  || vc == Character.class || vc == Byte.class 
+                         || vc == Integer.class || vc == Long.class  || vc == Short.class 
+                         || vc == Double.class  || vc == Float.class || vc == Boolean.class
+                         || vc == Class.class);
+                    // Nested?
+                    if (!simple)
+                    {   // Nested Class
+                        b.append(EOC);
+                        b.append(field.getName());
+                        b.append("[");
+                        b.append(clazz.getName());
+                        b.append("]=");
+                        b.append(EOL);
+                        appendConfigProperties(value.getClass(), value, b, false);
+                        return;
+                    }
+                }
+                // class info 
+                if (appendClassInfo)
+                {   appendClassInfo = false;
+                    b.append("[Properties of ");
+                    b.append(clazz.getName());
+                    b.append("]");
+                    b.append(EOL);
+                }
+                // property value
+                b.append(field.getName());
+                b.append("=");
+                b.append(String.valueOf(value));
+                b.append(EOL);
+            }
+            catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e)
+            {
+                log.warn("Field {} is ignored due to Exception {}", field.getName(), e.toString());
+            }
+        }
+        if (appendClassInfo==false)
+            b.append(EOC);
+    }
+    
+    /* helpers */
+    
+    private int findVarEnd(String value, int from)
+    {
+        final char NEST_CHAR = VAR_BEGIN.charAt(VAR_BEGIN.length()-1);
+        for (int nestCount = 0;from<value.length();from++)
+        {
+            char c = value.charAt(from);
+            if (c==VAR_END)
+            {   // Nested Variable
+                if (nestCount==0)
+                    return from;
+                nestCount--;
+            }
+            if (c==NEST_CHAR)
+                nestCount++;
+        }
+        return -1; // not found
     }
     
 	private void close(final Closeable closeable) {
@@ -244,7 +469,7 @@ public class XMLConfiguration
 			}
 			catch(IOException e)
 			{
-				log.debug(e.getMessage());
+				log.error("Failed to close stream: "+e.getMessage(), e);
 			}
 		}
 	}
